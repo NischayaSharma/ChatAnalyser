8e02ae32a3e5ee6e64a3e5c365a2e011e9322544
NischayaSharma <nischaya1703@gmail.com>
2019-11-27 21:58:55 +0530

    feat: Added Total no, of texts per sender by dataframe

--- chatOnlineSoln.py
+++ chatOnlineSoln.py
@@ -1,32 +1,45 @@
 import pandas as pd
+import itertools
+import sys
 import re
+
 
 def parse_file(text_file):
     file = open(text_file, 'r')
-    sender = []; message = []; datetime = []
+    sender = []
+    message = []
+    datetime = []
     for eachLine in file:
         if (re.search("^\d+/\d+/\d+, \d+:\d+ \w+ - ", eachLine)):
             data = eachLine
         else:
             data += eachLine
-
         datetime.append(data.split(' - ')[0])
-        if(len(data.split('-')[1].split(':'))>1):
+        if(len(data.split('-')[1].split(':')) > 1):
             s = data.split('-')[1].split(':')[0].strip()
             sender.append(s)
         else:
             sender.append('')
         try:
-            message.append(data.split(': ', 1)[1].replace('\n',' '))
+            message.append(data.split(': ', 1)[1].replace('\n', ' '))
         except:
             message.append('')
-    df = pd.DataFrame(zip(datetime, sender, message), columns=['timestamp', 'sender', 'message'])
+    df = pd.DataFrame(zip(datetime, sender, message), columns=[
+                      'timestamp', 'sender', 'message'])
     df['timestamp'] = pd.to_datetime(df['timestamp'])
-
-    # remove events not associated with a sender
     df = df[df.sender != ''].reset_index(drop=True)
-    
     return df
 
-dataDf = parse_file(raw_input("Enter the filename: "))
-print dataDf
+
+def mssgPerUser(df):
+    names = list(df['sender'].unique())
+    numOfTxts = []
+    for i in range(len(names)):
+        numOfTxts.append(0)
+    for name in df['sender']:
+        numOfTxts[names.index(name)] += 1
+    return names, numOfTxts
+
+
+dataDf = parse_file(sys.argv[1])
+print(mssgPerUser(dataDf))
