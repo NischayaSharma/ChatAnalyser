1da07fe601abef38e92e8f0732bdba5272c9a4fe
NischayaSharma <nischaya1703@gmail.com>
2019-11-29 09:16:06 +0530

    feat: Hinglish stopwords,trying sentiment analysi

--- chatOnlineSoln.py
+++ chatOnlineSoln.py
@@ -1,7 +1,21 @@
+from collections import Counter
+import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 import itertools
 import sys
 import re
+
+
+def plot_bar(df):
+    # this is for plotting purpose
+    index = np.arange(len(df['sender']))
+    plt.bar(index, df['numOfTxt'])
+    plt.xlabel('Sender', fontsize=13)
+    plt.ylabel('No of Texts', fontsize=13)
+    plt.xticks(index, df['sender'], fontsize=10, rotation=15)
+    plt.title('Texts sent by each sender in the convo')
+    plt.show()
 
 
 def parse_file(text_file):
@@ -21,13 +35,18 @@ def parse_file(text_file):
         else:
             sender.append('')
         try:
-            message.append(data.split(': ', 1)[1].replace('\n', ' '))
+            message.append(data.split(': ', 1)[1].replace('\n', ' ').strip())
         except:
             message.append('')
     df = pd.DataFrame(zip(datetime, sender, message), columns=[
                       'timestamp', 'sender', 'message'])
     df['timestamp'] = pd.to_datetime(df['timestamp'])
     df = df[df.sender != ''].reset_index(drop=True)
+    for i in range(len(df.message)):
+        if df.iloc[i]['message']== '<Media omitted>':
+            df.drop(df.index[i])
+            print "hey",df.index[i],i
+    print df
     return df
 
 
@@ -38,8 +57,29 @@ def mssgPerUser(df):
         numOfTxts.append(0)
     for name in df['sender']:
         numOfTxts[names.index(name)] += 1
-    return names, numOfTxts
+    df = pd.DataFrame(zip(names, numOfTxts), columns=['sender', 'numOfTxt'])
+    return df
 
 
 dataDf = parse_file(sys.argv[1])
-print(mssgPerUser(dataDf))
+dataDf['characters'] = dataDf.message.apply(len)
+dataDf['words'] = dataDf.message.apply(lambda x: len(x.split()))
+dataDf['keywords'] = dataDf.message.apply(lambda x: (x.split()))
+# print (dataDf)
+stopwords = []
+with open("stopwordsHinglish.txt") as file:
+    data = file.read()
+    stopwords = data
+
+for arr in dataDf['keywords']:
+    for keyword in stopwords:
+        if (keyword in arr):
+            arr.remove(keyword)
+# print dataDf
+
+# words = ''
+# for i in dataDf.message.values:
+#     words += '{} '.format(i.lower()) # make words lowercase
+
+# print (pd.DataFrame(Counter(words.split()).most_common(), columns=['word', 'frequency']))
+
